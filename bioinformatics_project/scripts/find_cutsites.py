e07df43b0368a0e8efaa49599452870b44646964
anya-decarlo <182010635+anya-decarlo@users.noreply.github.com>
2024-10-23 01:16:03 -0700

    commit

--- bioinformatics_project/scripts/find_cutsites.py
+++ bioinformatics_project/scripts/find_cutsites.py
@@ -4,10 +4,10 @@ import sys
 import argparse 
 
 #read FAFSTA file 
-def read_fasta(filepath):
-    with open( filepath, "r" ) as file:
+def read_fasta(fasta_file):
+    with open( fasta_file, "r" ) as file:
         lines = file.readlines()
-    sequence = "".join(line.strip() for line in lines if not line.startswitch(">"))
+    sequence = "".join(line.strip() for line in lines if not line.startswith(">"))
     return sequence 
 
 #find occurences of the cut sites 
@@ -30,8 +30,8 @@ def  find_pairs(locations):
                 pairs.append((locations[i], locations[j]))
     return pairs
 
-def save_summary_results(pairs, filepath):
-    with open(filepath, "w") as file:
+def save_summary_results(pairs, summary_file):
+    with open(summary_file, "w") as file:
         file.write("Total cut sites found:" + str(len(pairs)) + "\n")
         file.write("First 5 pairs:")
         for pair in pairs[:5]:
@@ -40,13 +40,11 @@ def save_summary_results(pairs, filepath):
 def main (): 
     fasta_file = sys.argv[1]
     cut_site = sys.argv[2]
+    summary_file = "/Users/anyadecarlo/05-first-exam-anya-decarlo/bioinformatics_project/results/cutsite_summary.txt"
     dna_sequence = read_fasta(fasta_file)
     positions = find_occurences(dna_sequence, cut_site)
     pairs = find_pairs(positions)
-    save_summary = pairs()
-    output = "/Users/anyadecarlo/05-first-exam-anya-decarlo/bioinformatics_project/results/cutsite_summary.txt"
-    save_results =(pairs, output)
-
+    save_summary = (pairs, summary_file)
 
 if __name__ == "main": 
     main ()
