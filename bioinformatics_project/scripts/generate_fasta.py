4f563ccda8fdf027e2884e409dc61e38daf4e457
anya-decarlo <182010635+anya-decarlo@users.noreply.github.com>
2024-10-22 17:12:36 -0700

    fasta

--- bioinformatics_project/scripts/generate_fasta.py
+++ bioinformatics_project/scripts/generate_fasta.py
@@ -4,17 +4,19 @@ import os
 import textwrap 
 
 #DNA sequence length with one million base pairs
-seq_length = 1,000,000
+seq_length = 1000000
 
 #DNA nucleotide bases 
 bases = ["A", "C" , "G", "T"]
 
 #Generates random DNA sequence with replacement 
-random_seq = "".join(random.choices)(populatin = bases, k = seq_length)
+random_seq = "".join(random.choices(population = bases, k = seq_length))
 
 #Format sequence with 80 basse pairs per line 
-format_seq = '\n'.join(random_seq[i:i +80] for i in range(0, len(random_seq), 80)
+format_seq = '\n'.join(
+    random_seq[i:i +80] for i in range(0, len(random_seq), 80)
 )
+
 
 
 
