9bdf1fdda0015dc1c24eeabd32a6270ddbb261e9
anya-decarlo <182010635+anya-decarlo@users.noreply.github.com>
2024-10-22 21:46:29 -0700

    dna_operations.py

---
+++ bioinformatics_project/scripts/dna_operations.py
@@ -0,0 +1,37 @@
+#import packages 
+import argparse 
+
+#DNA base complement dictionary 
+base_complement = {"A:T","T:A", "C:G", "G:C"}
+
+#complement sequence function
+#returns complement of a DNA Sequence
+def complement(sequence): 
+    complement_seq = "".join([base_complement[base] for base in sequence.upper()])
+    return complement_seq 
+
+#reverse sequence function 
+#returns the reverse of a sequence 
+
+def reverse(sequence):
+    return sequence[::-1]
+
+#reverse complement sequence function 
+#returns the reverse complement of a DNA sequence 
+
+def reverse_complement_sequence(sequence): 
+    return reverse(complement(sequence))
+
+def main(): 
+    parser = argparse.ArgumentParser
+    parser.add_argumet("sequence")
+    args = parser.pars_args()
+
+    #converts original sequence to uppercase 
+    original_sequence = args.sequence.upper()
+
+    #outputs results
+    print("Original sequence:      " ,original_sequence)
+    print("Complement:" , complement(original_sequence)) 
+    print("Reverse:       " ,reverse(original_sequence))
+    print("Reverse complement: ", reverse_complement_sequence(original_sequence))
